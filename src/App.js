b20fe3209ecf00265ee37878f86a74d8f53424aa
Sakthivel <velsakthi996@gmail.com>
2022-02-25 12:18:50 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Component/Home'
+import NavBar from './Component/NavBar/NavBar';
+import ClassComponent from './Component/ClassComponent';
+import FunctionComponent from './Component/FunctionComponent';
+import ErrorComponent from './Component/ErrorComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      {/* <header className="App-header"> */}
+        {/* <Home name="sakthi" no="234"> */}
+          {/* <p> Child Paragraph </p> */}
+        {/* </Home> */}
+        {/* <ClassComponent/> */}
+        {/* <FunctionComponent/> */}
+        {/* <ErrorComponent/> */}
+      {/* </header> */}
     </div>
   );
 }
